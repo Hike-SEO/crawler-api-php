95fd1e846608503438e16e57f54c54d609935259
ross-hike <ross@hikeseo.co>
2024-11-12 21:29:42 +0000

    Moved getting robots to service and changed from JSON response

--- app/Http/Controllers/Api/RobotsController.php
+++ app/Http/Controllers/Api/RobotsController.php
@@ -4,21 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RobotsRequest;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Http;
+use App\Services\RobotsService;
+use Illuminate\Http\Response;
 
 class RobotsController extends Controller
 {
-    public function __invoke(RobotsRequest $request): JsonResponse
+    public function __invoke(RobotsRequest $request, RobotsService $robotsService): Response
     {
-        $robotsUrl = parse_url($request->websiteUrl, PHP_URL_SCHEME)
-            .'://'.parse_url($request->websiteUrl, PHP_URL_HOST);
-
-        return response()->json([
-            Http::timeout(3)
-                ->withOptions(['allow_redirects' => ['max' => 2]])
-                ->get("{$robotsUrl}/robots.txt")
-                ->body(),
-        ]);
+        return response($robotsService->getRobotsTxt($request->websiteUrl));
     }
 }
--- tests/Feature/Http/Controllers/Api/RobotsControllerTest.php
+++ tests/Feature/Http/Controllers/Api/RobotsControllerTest.php
@@ -4,35 +4,26 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Http\Controllers\Api;
 
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Http;
+use App\Services\RobotsService;
 use Tests\TestCase;
 
 class RobotsControllerTest extends TestCase
 {
     public function test_will_return_robots_txt_for_website(): void
     {
-        Http::fake([
-            'https://example.com/robots.txt' => Http::response("User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php", 200),
-        ]);
+        $expectedResponse = "User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php";
+
+        $this->mock(RobotsService::class)
+            ->expects('getRobotsTxt')
+            ->with('https://example.com')
+            ->andReturn($expectedResponse);
 
         $this->usingTestApiToken()
             ->postJson('/api/crawl/robots', [
                 'website_url' => 'https://example.com',
             ])
             ->assertOk()
-            ->assertJson([
-                "User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php",
-            ]);
-
-        $this->usingTestApiToken()
-            ->postJson('/api/crawl/robots', [
-                'website_url' => 'https://example.com/page.html',
-            ])
-            ->assertOk()
-            ->assertJson([
-                "User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php",
-            ]);
+            ->assertSee($expectedResponse);
     }
 
     public function test_website_url_is_required(): void
@@ -45,54 +36,5 @@ class RobotsControllerTest extends TestCase
                     'The website url field is required.',
                 ],
             ]);
-    }
-
-    public function test_will_fail_if_timeout_is_longer_than_2_seconds(): void
-    {
-        Http::fake([
-            'https://example.com/robots.txt' => Http::response(null, Response::HTTP_REQUEST_TIMEOUT),
-        ]);
-
-        Http::shouldReceive('timeout')
-            ->withArgs(['3'])
-            ->once();
-
-        $this->usingTestApiToken()
-            ->postJson('/api/crawl/robots', [
-                'website_url' => 'https://example.com',
-            ])
-            ->assertServerError();
-    }
-
-    public function test_will_handle_redirect(): void
-    {
-        Http::fake([
-            'https://example.com/robots.txt' => Http::response(null, 302, ['Location' => 'https://example.com/other-location']),
-            'https://example.com/other-location' => Http::response("User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php"),
-        ]);
-
-        $this->usingTestApiToken()
-            ->postJson('/api/crawl/robots', [
-                'website_url' => 'https://example.com',
-            ])
-            ->assertOk()
-            ->assertJson([
-                "User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php",
-            ]);
-    }
-
-    public function test_will_fail_if_too_many_redirects(): void
-    {
-        Http::fake([
-            'https://example.com/robots.txt' => Http::response(null, 302, ['Location' => 'https://example.com/redirect1']),
-            'https://example.com/redirect1' => Http::response(null, 302, ['Location' => 'https://example.com/redirect2']),
-            'https://example.com/redirect2' => Http::response(null, 302, ['Location' => 'https://example.com/redirect3']),
-        ]);
-
-        $this->usingTestApiToken()
-            ->postJson('/api/crawl/robots', [
-                'website_url' => 'https://example.com',
-            ])
-            ->assertServerError();
     }
 }
