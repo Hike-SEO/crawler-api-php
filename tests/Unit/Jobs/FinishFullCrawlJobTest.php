bbef77c66f928bbc0b1e031c412ee16ea29693d9
ross-hike <ross@hikeseo.co>
2024-11-28 22:47:43 +0000

    Added test todos

---
+++ tests/Unit/Jobs/FinishFullCrawlJobTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Unit\Jobs;
+
+use Tests\TestCase;
+
+class FinishFullCrawlJobTest extends TestCase
+{
+    // TODO
+}
---
+++ tests/Unit/Jobs/StartFullCrawlJobTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Unit\Jobs;
+
+use Tests\TestCase;
+
+class StartFullCrawlJobTest extends TestCase
+{
+    // TODO
+}
